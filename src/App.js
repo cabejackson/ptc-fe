595cf855286746c6ea9b6d2784934399bb6c5286
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2023-09-27 09:32:59 -0400

    Start of react router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import './App.css';
-import axios from "axios";
-import Cards from "./components/Cards/cards"
-import { useEffect, useState } from 'react';
+// import axios from "axios";
+// import Cards from "./components/Cards/cards"
+// import { useEffect, useState } from 'react';
+// import {jsx as _jsx} from 'react/jsx-runtime';
+import AppBar from './components/AppBar/AppBar';
 
-const API_URL = "http://localhost:3000/api/v1/cards";
+// const API_URL = "http://localhost:3000/api/v1/cards";
 
-function getAPIData() {
-  return axios.get(API_URL).then((response) => response.data)
-}
+// function getAPIData() {
+//   return axios.get(API_URL).then((response) => response.data)
+// }
 
 
 function App() {
-  const [cards, setCards] = useState([]);
+  // const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    let mounted = true;
-    getAPIData().then((items) => {
-      if(mounted){
-        setCards(items);
-      }
-    })
-  return () => { (mounted = false) }
-  }, []);
+  // useEffect(() => {
+  //   let mounted = true;
+  //   getAPIData().then((items) => {
+  //     if(mounted){
+  //       setCards(items);
+  //     }
+  //   })
+  // return () => { (mounted = false) }
+  // }, []);
 
   return (
     <div className="App">
-      <h1>hello world</h1>
-      <Cards cards={cards}/>
+      {/* <h1>hello world</h1> */}
+      <AppBar />
+      {/* <Cards cards={cards}/> */}
     </div>
   );
 }
--- src/components/Cards/cards.js
+++ src/components/Cards/cards.js
@@ -1,19 +1,20 @@
-import React from 'react'
+// import React from 'react'
+// import {jsx as _jsx} from 'react/jsx-runtime';
 
-function Cards(props) {
-  return <div>
-       <h1>Cards from the API</h1>
-       {props.cards.map((card) => {
-        return <div key={card.id}>
-            <h2>{card.title}</h2>
-            <h2>{card.target}</h2>
-            <p>{card.user_story}</p>
-        </div>
-    })}
-    </div>
-}
+// function Cards(props) {
+//   return <div>
+//        <h1>Cards from the API</h1>
+//        {props.cards.map((card) => {
+//         return <div key={card.id}>
+//             <h2>{card.title}</h2>
+//             <h2>{card.target}</h2>
+//             <p>{card.user_story}</p>
+//         </div>
+//     })}
+//     </div>
+// }
 
-export default Cards
+// export default Cards
 
 
 
